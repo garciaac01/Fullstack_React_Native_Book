2530225b4b10d78219f5021456701b29c79cb9a0
garciaac01 <garciaac01@gmail.com>
2020-04-19 20:45:25 -0400

    Wire up creation of new timers

--- time-tracking/App.js
+++ time-tracking/App.js
@@ -4,6 +4,7 @@ import uuidv4 from 'uuid/v4';
 
 import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
+import { newTimer } from './utils/TimerUtils';
 
 export default class App extends React.Component {
   state = {
@@ -25,6 +26,14 @@ export default class App extends React.Component {
     ],
   };
 
+  handleCreateFormSubmit = timer => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  };
+
   render() {
     const { timers } = this.state;
 
@@ -34,7 +43,9 @@ export default class App extends React.Component {
           <Text style={styles.title}>Timers</Text>
         </View>
         <ScrollView style={styles.timerList}>
-          <ToggleableTimerForm isOpen={false} />
+          <ToggleableTimerForm
+            onFormSubmit={this.handleCreateFormSubmit}
+          />
           {timers.map(({ title, project, id, elapsed, isRunning }) => (
             <EditableTimer
               key={id}
